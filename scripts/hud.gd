1e0a1991d212c9a04319a2424848071465b56937
JL <74738871+JLi69@users.noreply.github.com>
2025-08-01 10:30:56 -0500

    started work on pause screen

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready() -> void:
+	$PauseScreen.hide()
+	$DeathScreen.hide()
+
 func _process(_delta: float) -> void:
 	var level_loaded = get_node_or_null("/root/Main/Level")
 	var player: Player = get_node_or_null("/root/Main/Level/PlayerSatellite")
@@ -13,7 +17,15 @@ func _process(_delta: float) -> void:
 		$DamageFlash.hide()
 
 	# Player is dead
-	if level_loaded and !player:
+	if level_loaded and !player and !$PauseScreen.visible:
 		$DeathScreen.show()
 	else:
 		$DeathScreen.hide()
+
+	if level_loaded:
+		if Input.is_action_just_pressed("ui_cancel"):
+			$PauseScreen.visible = !$PauseScreen.visible
+			get_tree().paused = $PauseScreen.visible
+	else:
+		$PauseScreen.visible = false
+		get_tree().paused = false
